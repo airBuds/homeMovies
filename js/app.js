b0fb49c4f2d836a1ddb9ee6a3437ef969e7ea1c1
Tinedanzer <johnhillallen@gmail.com>
2018-07-21 12:39:01 -0500

    final

--- js/app.js
+++ js/app.js
@@ -63,3 +63,10 @@ $(document).ready(function () {
     });
 
 });
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
