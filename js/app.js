3f4124a1e7f8018fe6b9c25830f2af83c128782a
DanielOhly <38668083+DanielOhly@users.noreply.github.com>
2018-07-24 20:43:14 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -76,6 +76,7 @@ $(document).ready(function () {
               $("#menu-toggle").click(function (e) {
                 e.preventDefault();
                 $("#wrapper").toggleClass("toggled");
+                  console.log("Hello");
             });
     
     
